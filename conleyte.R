21a43984f362bee1a402c33a266020338ee6aa57
Michelle Stuart <bunx21@yahoo.com>
2017-01-05 13:43:15 -0500

    labor -> leyte

--- conleyte.R
+++ conleyte.R
@@ -3,5 +3,5 @@
 conleyte <- function(){
   suppressMessages(library(dplyr))
   leyte <- src_mysql(dbname = "Leyte", default.file = path.expand("~/myconfig.cnf"), port = 3306, create = F, host = NULL, user = NULL, password = NULL)
-  return(labor)
+  return(leyte)
 }
