2f202a6b8125d742118fd0511d919bab6b872615
Michelle Stuart <bunx21@yahoo.com>
2017-01-05 13:42:08 -0500

    fix type-o conlabor -> conleyte

--- conleyte.R
+++ conleyte.R
@@ -1,6 +1,6 @@
 # a function to connect to the Leyte database
 
-conlabor <- function(){
+conleyte <- function(){
   suppressMessages(library(dplyr))
   leyte <- src_mysql(dbname = "Leyte", default.file = path.expand("~/myconfig.cnf"), port = 3306, create = F, host = NULL, user = NULL, password = NULL)
   return(labor)
