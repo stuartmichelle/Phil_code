f84939ebd14fe4a079557a6484359091eb1c9e30
Michelle Stuart <Whitmore@vpn-client-172-16-9-53.rutgers.edu>
2017-01-10 11:58:00 -0500

    working

--- collectorder.R
+++ collectorder.R
@@ -5,3 +5,14 @@
 source("conleyte.R")
 
 leyte <- conleyte()
+
+# for all of the dives from 2015_05 season
+c1 <- leyte %>% tbl("diveinfo")  %>% filter(date > "2015-04-01" & date < "2016-01-01")  %>% select(id, date)
+
+# for all anem obs from those dives
+c2 <- leyte %>% tbl("anemones") 
+c3 <- left_join(c1, c2, by = c("id" = "dive_table_id"))  %>% select(anem_table_id, ObsTime, date) %>% collect()
+
+# find all fish from that season in order of time collected
+c4 <- leyte %>% tbl("clownfish")
+fish <- left_join(c3, c4, by = "anem_table_id", copy = T)  %>% select(ObsTime, sample_id, date)
