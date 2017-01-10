a2fa741a0979068b78e2e6df0d49768f13f35d98
Michelle Stuart <Whitmore@Michelles-MacBook.local>
2017-01-10 11:32:04 -0500

    init

---
+++ collectorder.R
@@ -0,0 +1,7 @@
+# a script to put samples in order of collection - this is for the 2015_05 field
+# season where samples were not identified by order of collection but by tag
+# number.
+
+source("conleyte.R")
+
+leyte <- conleyte()
